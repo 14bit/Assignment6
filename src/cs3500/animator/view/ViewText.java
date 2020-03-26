bbee6c4873645232da351dbfda0e75cdb57095d7
bltdude <62484809+bltdude@users.noreply.github.com>
2020-03-26 15:32:34 -0700

    Update ViewText.java

--- src/cs3500/animator/view/ViewText.java
+++ src/cs3500/animator/view/ViewText.java
@@ -30,7 +30,32 @@ public class ViewText implements ExcellenceView {
   @Override
   public void go(PrintStream out) {
     out.append("We are writing directly to a file!\n");
-    //getChanges();
+    
+    out.append("canvas" + canvasInfo[0].toString() + canvasInfo[1].toString() + canvasInfo[2].toString() + canvasInfo[3].toString() + "\n");
+   
+    for (Shape s : model.returnShapeList()) {
+      if (s instanceof Rectangle) {
+        textString.append("shape" + s.name + "rectangle" + "\n");
+      } else if (s instanceof Ellipse) {
+        textString.append("shape" + s.name + "ellipse" + "\n");
+      }
+      //if there are movements
+      if (s.changes.size() > 0) {
+        //for each change in the shape
+        for (int k = 0; k < s.changes.size(); k++) {
+          //write that there's a motion belonging to this shape...
+          textString.append("motion " + s.name);
+          //...then write each number...
+          for (int j : s.changes.get(k)) {
+            textString.append(" " + j);
+          }
+          //...and then go to a new line when done
+          textString.append("\n");
+        }
+      }
+    }
     System.exit(0);
+  }
+}
   }
 }
