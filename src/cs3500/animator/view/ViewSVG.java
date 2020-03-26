5291d5d190f4a1453c6f77dd15f6c79ac0f8aaa8
bltdude <62484809+bltdude@users.noreply.github.com>
2020-03-26 16:56:09 -0700

    Update ViewSVG.java

--- src/cs3500/animator/view/ViewSVG.java
+++ src/cs3500/animator/view/ViewSVG.java
@@ -13,10 +13,13 @@ import java.io.PrintStream;
  */
 public class ViewSVG implements ExcellenceView {
 
+  int[] canvasInfo;
   private final ExcellenceOperations model;
 
   ViewSVG(ExcellenceOperations model) {
     this.model = model;
+    
+    this.canvasInfo = model.getCanvasInfo();
   }
 
   @Override
@@ -31,9 +34,14 @@ public class ViewSVG implements ExcellenceView {
 
   @Override
   public void go(PrintStream out) {
-
+    out.append("<svg width=\"" + canvasInfo[0] + "\" height=\"" + canvasInfo[4] + " version=\"1.1\" " + "xmnls=\"http://www.w3.org/2000/svg\">"
     //for every shape
     for (Shape s : model.returnShapeList()) {
+      if (s instanceof Rectangle) {
+        out.append("<rect id=\"" + s.name + "\" x=\"" + s.x1 + "\" y=\"" + s.y1 + "\" width=\" + s.w1 + "\" height=\"" + s.h1 + "\" fill=\"rgb(" + s.r1 + "." + s.g1 + "." + s.b1 + ")\" visibility=\"visible\" >" + \n");
+      } else if (s instanceof Ellipse) {
+        out.append("<ellipse id=\"" + s.name + "\" cx=\"" + s.x1 + "\" cy=\"" + s.y1 + "\" rx=\" + s.w1 + "\" ry=\"" + s.h1 + "\" fill=\"rgb(" + s.r1 + "." + s.g1 + "." + s.b1 + ")\" visibility=\"visible\" >" + \n");
+      }
       //if there are movements
       if (s.changes.size() > 0) {
         //for each change in the shape
@@ -49,9 +57,5 @@ public class ViewSVG implements ExcellenceView {
         }
       }
     }
-
-
   }
-
-
 }
